38a2cc8cd525a7c322228b58cc6e504f9738b922
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-03-14 14:40:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+Vue.createApp({
+
+    data() {
+
+        return {
+            
+            r: 255, // Startværdi for rød
+            g: 0,   // Startværdi for grøn
+            b: 0    // Startværdi for blå
+        }
+
+    },
+    computed: {
+        
+    },
+    methods: {
+
+    }
+
+}).mount("#app")
